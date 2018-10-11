b1507be7e1faf1761de7a0578d534b7083e97cfb
yafeunteun <yannfeunteun@gmail.com>
2018-10-11 15:08:12 +0200

    Post message to Slack when lambda function is triggered.

--- notifier/handler.py
+++ notifier/handler.py
@@ -1,11 +1,16 @@
 import os
+import requests 
 
 
 def post_to_slack(event, context):
 
     slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
 
-    print (slack_webhook_url)
+    slack_message = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
+    
+    data = {"text": slack_message}
+    
+    requests.post(slack_webhook_url, json=data)
 
     return
 
